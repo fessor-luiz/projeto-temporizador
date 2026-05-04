ad938bd676ef668ac609c158eb96a6d96f545367
Professor Luiz Henrique <luizegydio@professor.educacao.sp.gov.br>
2026-05-04 16:53:01 -0300

    logica aumentar diminuir tempo

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const tempo = document.getElementById('tempo')
+const diminuirBtn = document.getElementById('diminuir')
+const aumentarBtn = document.getElementById('aumentar')
+const valorAtualInput = document.getElementById('valor-atual')
 
 let tempoRestante = 1000000000
+let valorAtual = 55
+
+diminuirBtn.addEventListener('click', () => {
+    if (valorAtual - 10 <= 0) {
+        valorAtual = 1
+    } else {
+        valorAtual -= 10
+    }
+
+    valorAtualInput.value = valorAtual
+})
+
+aumentarBtn.addEventListener('click', () => {
+    valorAtual += 10
+
+    valorAtualInput.value = valorAtual
+})
 
 setInterval(() => {
 
