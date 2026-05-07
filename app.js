4cefe3ea8ad1dba14a83650c45a735705c6efe92
Professor Luiz Henrique <luizegydio@professor.educacao.sp.gov.br>
2026-05-07 16:54:35 -0300

    logica de zerar e adicionar tempo

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const tempo = document.getElementById('tempo')
 const diminuirBtn = document.getElementById('diminuir')
 const aumentarBtn = document.getElementById('aumentar')
 const valorAtualInput = document.getElementById('valor-atual')
+const botaoZerar = document.getElementById('botao-zerar')
+const botaoAdd = document.getElementById('botao-add')
 
-let tempoRestante = 1000000000
-let valorAtual = 55
+let tempoRestante = 0
+let valorAtual = 0
+
+
+botaoZerar.onclick = () => {
+    tempoRestante = 0
+}
+
+botaoAdd.onclick = () => {
+    let valor = valorAtualInput.value
+    tempoRestante += valor
+}
 
 diminuirBtn.addEventListener('click', () => {
     if (valorAtual - 10 <= 0) {
@@ -23,7 +35,12 @@ aumentarBtn.addEventListener('click', () => {
 })
 
 setInterval(() => {
+    if (tempoRestante <= 0) {
+        tempoRestante = 0
+    } else {
+        tempo.innerText = `${tempoRestante} segs`
+        tempoRestante = tempoRestante - 1
 
-    tempo.innerText = `${tempoRestante} segs`
-    tempoRestante = tempoRestante - 1
+    }
+
 }, 1000)
